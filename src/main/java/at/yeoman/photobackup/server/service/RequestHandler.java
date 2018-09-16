d5fcd15bb69a25fa346e1b297b614e11bad46ac2
sbohmann <mail@sebastian-bohmann.com>
2018-09-16 09:23:24 +0200

    Filtering missing assets

--- src/main/java/at/yeoman/photobackup/server/service/RequestHandler.java
+++ src/main/java/at/yeoman/photobackup/server/service/RequestHandler.java
@@ -5,20 +5,16 @@ import at.yeoman.photobackup.server.api.Checksum;
 import at.yeoman.photobackup.server.api.MissingAssets;
 import at.yeoman.photobackup.server.api.ResourceDescription;
 import at.yeoman.photobackup.server.core.Core;
-import com.oracle.tools.packager.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.nio.channels.FileLock;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,16 +22,23 @@ import java.util.stream.Collectors;
 public class RequestHandler {
     private final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Core core;
-    private File photoDirectory;
+    private final Core core;
+    private final File uploadDirectory;
+    private final File photoDirectory;
 
     @Autowired
     RequestHandler(Core core) throws IOException {
         this.core = core;
-        photoDirectory = new File("photos");
-        if (!photoDirectory.isDirectory()) {
-            throw new IOException("Not a dirsctory: " + photoDirectory.getCanonicalPath());
+        uploadDirectory = getDirectory("upload");
+        photoDirectory = getDirectory("photos");
+    }
+
+    private static File getDirectory(String name) throws IOException {
+        File result = new File(name);
+        if (!result.isDirectory()) {
+            throw new IOException("Not a dirsctory: " + result.getCanonicalPath());
         }
+        return result;
     }
 
     @GetMapping("/")
@@ -54,6 +57,7 @@ public class RequestHandler {
                 .stream()
                 .flatMap(x -> x.getResourceDescriptions().stream())
                 .map(ResourceDescription::getChecksum)
+                .filter(this::missing)
                 .distinct()
                 .collect(Collectors.toList());
         result.setMissingAssetChecksums(checksums);
@@ -61,49 +65,126 @@ public class RequestHandler {
         return result;
     }
 
+    private boolean missing(Checksum checksum) {
+        try {
+            return backupExists(checksum) == false;
+        } catch (Exception exception) {
+            log.error(exception.getMessage(), exception);
+            return true;
+        }
+    }
+
+    private boolean backupExists(Checksum checksum) throws Exception {
+        File file = new File(photoDirectory, fileNameForChecksum(checksum));
+        return file.isFile() && checksumForFile(file).equals(checksum);
+    }
+
     @PostMapping("/resource-upload/{checksumString}")
     @ResponseBody
     public ResponseEntity<String> handleResourceUpload(@PathVariable final String checksumString, InputStream bodyStream) throws Exception {
-        final Checksum checksumFromPath;
         try {
-            checksumFromPath = new Checksum(checksumString);
+            final Checksum checksumFromPath;
+            try {
+                checksumFromPath = new Checksum(checksumString);
+            } catch (Exception exception) {
+                log.error(exception.getMessage(), exception);
+                return error("Illegal checksum string from path [" + checksumString + "]",
+                        HttpStatus.BAD_REQUEST);
+            }
+            String fileName = fileNameForChecksum(checksumFromPath);
+            File uploadTarget = new File(uploadDirectory, fileName);
+            final Checksum calculatedChecksum = writeFile(bodyStream, uploadTarget);
+            if (!calculatedChecksum.equals(checksumFromPath)) {
+                return error("Checksum mismatch - checksum from request path: " + checksumFromPath +
+                        ", calculated: " + calculatedChecksum,
+                        HttpStatus.EXPECTATION_FAILED);
+            }
+            File renamedTarget = new File(photoDirectory, fileName);
+            if (renamedTarget.exists()) {
+                if (!renamedTarget.isFile()) {
+                    return error("renamed target [" + renamedTarget.getCanonicalPath() + "] is not a file",
+                            HttpStatus.EXPECTATION_FAILED);
+                } else if (renamedTarget.length() > uploadTarget.length()) {
+                    return error("renamed target [" + renamedTarget.getCanonicalPath() + "] length [" + renamedTarget.length() + "]" +
+                            " > upload target [" + uploadTarget.getCanonicalPath() + "] length [" + uploadTarget.length() + "]",
+                            HttpStatus.EXPECTATION_FAILED);
+                } else if (renamedTarget.length() == uploadTarget.length() && checksumForFile(uploadTarget).equals(checksumFromPath)) {
+                    if (!uploadTarget.delete()) {
+                        log.error("Unable to delete upload target while retaining existing backup [" + uploadTarget.getCanonicalPath() + "]");
+                    }
+                    return success("Retaining matching file [" + renamedTarget.getCanonicalPath());
+                } else {
+                    if (!renamedTarget.delete()) {
+                        return error("Unable to delete non-matching renamed target [" + renamedTarget.getCanonicalPath() + "]",
+                                HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                    if (!uploadTarget.renameTo(renamedTarget)) {
+                        return error("Unable to rename [" + uploadTarget.getCanonicalPath() + "] to [" + renamedTarget.getCanonicalPath() + "]",
+                                HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                }
+            }
+            return error(calculatedChecksum.toString(), HttpStatus.OK);
         } catch (Exception exception) {
-            return new ResponseEntity<>("Illegal checksum string from path [" + checksumString + "]",
-                    HttpStatus.BAD_REQUEST);
-        }
-        File target = new File(photoDirectory,  checksumString + ".jpg");
-        final Checksum calculatedChecksum = writeFile(bodyStream, target);
-        if (!calculatedChecksum.equals(checksumFromPath)) {
-            return new ResponseEntity<>("Checksum mismatch - checksum from request path: " + checksumFromPath +
-                    ", calculated: " + calculatedChecksum,
+            log.error(exception.getMessage(), exception);
+            return error("Error message: [" + exception.getMessage() + "]",
                     HttpStatus.EXPECTATION_FAILED);
         }
-        return new ResponseEntity<>(calculatedChecksum.toString(), HttpStatus.OK);
+    }
+
+    private String fileNameForChecksum(Checksum checksum) {
+        return checksum.getValue().toRawString() + ".jpg";
+    }
+
+    private ResponseEntity<String> success(String message) {
+        log.error(message);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> error(String message, HttpStatus status) {
+        log.error(message);
+        return new ResponseEntity<>(message, status);
     }
 
     private Checksum writeFile(InputStream in, File target) throws Exception {
         log.info("writing to file [" + target.getCanonicalPath() + "]...");
-        FileOutputStream out = new FileOutputStream(target);
-        MessageDigest md = MessageDigest.getInstance("SHA-512");
-        byte[] buffer = new byte[1024 * 1024];
-        long bytesWritten = 0;
-        while (true) {
-            int n = in.read(buffer);
-            if (n < 0) {
-                break;
+        try (FileOutputStream out = new FileOutputStream(target);
+             FileLock lock = out.getChannel().lock()) {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] buffer = new byte[1024 * 1024];
+            long bytesWritten = 0;
+            while (true) {
+                int n = in.read(buffer);
+                if (n < 0) {
+                    break;
+                }
+                out.write(buffer, 0, n);
+                md.update(buffer, 0, n);
+                bytesWritten += n;
             }
-            out.write(buffer, 0, n);
-            md.update(buffer, 0, n);
-            bytesWritten += n;
+            out.getChannel().force(true);
+            try {
+                out.getFD().sync();
+            } catch (SyncFailedException exception) {
+                log.debug("Unable to sync file [" + target.getCanonicalPath() + "]", exception);
+            }
+            log.info("finished writing " + bytesWritten + " bytes to file [" + target.getCanonicalPath() + "].");
+            return new Checksum(md.digest());
         }
-        out.getChannel().force(true);
-        out.close();
-        try {
-            out.getFD().sync();
-        } catch (SyncFailedException exception) {
-            log.debug("Unable to sync file [" + target.getCanonicalPath() + "]", exception);
+    }
+
+    private Checksum checksumForFile(File file) throws Exception {
+        try (FileInputStream in = new FileInputStream(file)) {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] buffer = new byte[1024 * 1024];
+            while (true) {
+                int n = in.read(buffer);
+                if (n < 0) {
+                    break;
+                }
+                md.update(buffer, 0, n);
+            }
+            return new Checksum(md.digest());
         }
-        log.info("finished writing " + bytesWritten + " bytes to file [" + target.getCanonicalPath() + "].");
-        return new Checksum(md.digest());
     }
 }
