c4c3cabf8ab0daafc9f069d9719be69b70209f97
sbohmann <mail@sebastian-bohmann.com>
2018-09-19 14:49:25 +0200

    ignored now-empty core test

--- src/test/java/at/yeoman/photobackup/server/CoreTest.java
+++ src/test/java/at/yeoman/photobackup/server/CoreTest.java
@@ -2,6 +2,7 @@ package at.yeoman.photobackup.server;
 
 import at.yeoman.photobackup.server.configuration.CoreConfiguration;
 import at.yeoman.photobackup.server.core.Core;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CoreTest {
