5a22657d940cb1c621e21ae025bd34287ce8d7b1
sbohmann <mail@sebastian-bohmann.com>
2018-09-29 14:01:38 +0200

    Started building gallery page

--- src/main/java/at/yeoman/photobackup/server/gallery/GalleryRequestHandler.java
+++ src/main/java/at/yeoman/photobackup/server/gallery/GalleryRequestHandler.java
@@ -1,4 +1,16 @@
 package at.yeoman.photobackup.server.gallery;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@Controller
 public class GalleryRequestHandler {
+    @GetMapping("/gallery")
+    public String greeting(Model model) {
+        model.addAttribute("date", LocalDate.now());
+        return "gallery";
+    }
 }
