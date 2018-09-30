71692c311067bceae252f28a45d39124b22cfdd6
sbohmann <mail@sebastian-bohmann.com>
2018-09-30 16:38:02 +0200

    Integrated js-jods from npm web jar; added resource handler and encoding interceptor for delivering the scripts from their resource path

--- src/main/java/at/yeoman/photobackup/server/configuration/CoreConfiguration.java
+++ src/main/java/at/yeoman/photobackup/server/configuration/CoreConfiguration.java
@@ -1,4 +1,33 @@
 package at.yeoman.photobackup.server.configuration;
 
-public class CoreConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class CoreConfiguration implements WebMvcConfigurer {
+    private static final String JsJodaPath = "/js-joda/";
+    private static final String JsJodaResourcePath = "/META-INF/resources/webjars/js-joda/1.8.2/dist/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js-joda/**").addResourceLocations("classpath:" + JsJodaResourcePath);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                response.setCharacterEncoding("UTF-8");
+                return true;
+            }
+        });
+    }
 }
