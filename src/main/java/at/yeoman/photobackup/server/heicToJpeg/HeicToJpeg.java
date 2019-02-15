e4179a24bc22060538bd13eaa67914b533236916
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-02-15 14:47:22 +0100

    Successfully produced a crash with JNI and ImageMagick after resolving all missing libraries.

--- src/main/java/at/yeoman/photobackup/server/heicToJpeg/HeicToJpeg.java
+++ src/main/java/at/yeoman/photobackup/server/heicToJpeg/HeicToJpeg.java
@@ -1,5 +1,22 @@
 package at.yeoman.photobackup.server.heicToJpeg;
 
+import java.io.File;
+
 public class HeicToJpeg {
+    static {
+        System.loadLibrary("CORE_RL_zlib_");
+        System.loadLibrary("CORE_RL_glib_");
+        System.loadLibrary("CORE_RL_bzlib_");
+        System.loadLibrary("CORE_RL_lcms_");
+        System.loadLibrary("CORE_RL_libxml_");
+        System.loadLibrary("CORE_RL_lqr_");
+        System.loadLibrary("CORE_RL_ttf_");
+        System.loadLibrary("CORE_RL_zlib_");
+        System.loadLibrary("CORE_RL_MagickCore_");
+        System.loadLibrary("CORE_RL_MagickWand_");
+        System.loadLibrary("CORE_RL_Magick++_");
+        System.loadLibrary("photobackup_server_native");
+    }
+
     native static byte[] convert(byte[] heicData);
 }
