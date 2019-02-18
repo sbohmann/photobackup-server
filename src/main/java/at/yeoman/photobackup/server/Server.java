0708fc0cc2e741e05d17f27e5a806fd43044cb84
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-02-18 11:32:48 +0100

    Using a file lock to prevent parallel server instances

--- src/main/java/at/yeoman/photobackup/server/Server.java
+++ src/main/java/at/yeoman/photobackup/server/Server.java
@@ -3,9 +3,17 @@ package at.yeoman.photobackup.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+
 @SpringBootApplication
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileLock lock = new RandomAccessFile("photobackup-server.lock", "rw").getChannel().tryLock();
+        if (lock == null) {
+            throw new IllegalStateException("Unable to aquire file lock");
+        }
         SpringApplication.run(Server.class, args);
     }
 }
