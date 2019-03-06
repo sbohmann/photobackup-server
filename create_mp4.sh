f9c667f64b8e8b3a3a0588b0f7804f218f511751
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-03-06 16:42:14 +0100

    MP4/H264 conversion of MOV videos

--- create_mp4.sh
+++ create_mp4.sh
@@ -2,4 +2,6 @@
 checksum="$1"
 input_file="photos/${checksum}"
 output_file="videos/${checksum}.mp4"
-echo ffmpeg -i "${input_file}" "${output_file}"
+echo "Converting input file [${input_file}] to mp4 output file [${output_file}]..."
+ffmpeg -i "${input_file}" "${output_file}" || exit $?
+echo "Finished onverting input file [${input_file}] to mp4 output file [${output_file}]."
