9f0232a11f04759bdc1db0eea8885dcfff414715
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-03-07 09:46:41 +0100

    MP4/H264 conversion of MOV videos

--- create_mp4.sh
+++ create_mp4.sh
@@ -6,18 +6,18 @@ output_file="videos/${checksum}.mp4"
 
 if test -e "${output_file}"; then exit 21; fi
 
-attempt_file="${output_file}.attempt"
+temporary_file="videos/${checksum}.temporary.mp4"
 
-if test -f "${attempt_file}" && test -w "${attempt_file}"; then rm "${attempt_file}" || exit 22; fi
+if test -f "${temporary_file}" && test -w "${temporary_file}"; then rm "${temporary_file}" || exit 22; fi
 
-if test -e "${attempt_file}"; then exit 23; fi
+if test -e "${temporary_file}"; then exit 23; fi
 
 echo "Converting input file [${input_file}] to mp4 output file [${output_file}]..."
 
-ffmpeg -n -i "${input_file}" "${attempt_file}"; result = "$?"
+ffmpeg -n -i "${input_file}" "${temporary_file}"; result = "$?"
 
 if ! test "$result" == "0"; then (echo "ffmpeg failed with exit code ${result}" >&2); exit 24; fi
 
-mv "${attempt_file}" "${output_file}" || exit 25
+mv "${temporary_file}" "${output_file}" || exit 25
 
 echo "Finished converting input file [${input_file}] to mp4 output file [${output_file}]."
