fdb064c0983e96421031c10a7abcaa170aa701c6
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-03-07 12:29:20 +0100

    MP4/H264 conversion of MOV videos

--- src/main/resources/static/scripts/gallery/gallery.js
+++ src/main/resources/static/scripts/gallery/gallery.js
@@ -160,18 +160,24 @@ function createThumbnailPlayer(resource, div) {
     if (resource.name.match(/.*\.(mov|mp4)/i) == null) {
         return
     }
-    let video = document.createElement('video')
-    video.width = 200
-    video.height = 200
-    video.controls = true
-    video.preload = 'none'
-    let mp4Source = document.createElement('source')
-    mp4Source.src = '/videos/' + resource.checksum + '/' + resource.name
-    video.appendChild(mp4Source)
-    let rawSource = document.createElement('source')
-    rawSource.src = '/photos/' + resource.checksum + '/' + resource.name
-    video.appendChild(rawSource)
-    div.appendChild(video)
+    let playButton = document.createElement('img')
+    playButton.src = '/images/gallery/play_button.png'
+    playButton.classList.add('play-button')
+    div.appendChild(playButton)
+    playButton.onclick = () => {
+        let video = document.createElement('video')
+        video.width = 200
+        video.height = 200
+        video.controls = true
+        video.autoplay = true
+        let mp4Source = document.createElement('source')
+        mp4Source.src = '/videos/' + resource.checksum + '/' + resource.name
+        video.appendChild(mp4Source)
+        let rawSource = document.createElement('source')
+        rawSource.src = '/photos/' + resource.checksum + '/' + resource.name
+        video.appendChild(rawSource)
+        div.replaceChild(video, playButton)
+    }
 }
 
 function createLink(resource, div, name, address) {
