99a90d7532ee446b98d6f9045992d26b13b69046
sbohmann <mail@sebastian-bohmann.com>
2019-03-28 14:44:46 +0100

    MOved testing endpoint oackage so spring mvc finds it

--- src/main/java/at/yeoman/photobackup/server/upload_exploration/UploadExplorationRequestHandler.java
+++ src/main/java/at/yeoman/photobackup/server/upload_exploration/UploadExplorationRequestHandler.java
@@ -1,4 +1,4 @@
-package upload_exploration;
+package at.yeoman.photobackup.server.upload_exploration;
 
 import at.yeoman.photobackup.server.assets.Checksum;
 import org.slf4j.Logger;
@@ -15,7 +15,7 @@ import java.security.MessageDigest;
 public class UploadExplorationRequestHandler {
     private final Logger log = LoggerFactory.getLogger(UploadExplorationRequestHandler.class);
 
-    @PostMapping("/upload_exploration")
+    @PostMapping("/at/yeoman/photobackup/server/upload_exploration")
     public ResponseEntity<String> handleResourceUpload(InputStream bodyStream) {
         try {
             final Checksum calculatedChecksum = processInputStream(bodyStream);
