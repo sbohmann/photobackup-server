faa72cd15719e2cd67f4296acd8c5294537d2413
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-10 14:03:29 +0200

    Added warning to almost finished Dockerfile still using ephemeral storage

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-clear; docker build -t photobackup-server . && docker run -it --rm photobackup-server
+#!/usr/bin/env bash
+
+if [[ ! -d "$1" ]]
+then
+  echo "not a directory: [$1]"
+  exit 1
+fi
+
+clear; docker build -t photobackup-server . && docker run -it --rm photobackup-server -v "$1":/storage
