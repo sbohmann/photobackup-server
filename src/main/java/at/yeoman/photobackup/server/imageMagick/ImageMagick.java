83f10062b5a2b977a4583c45d18aea3dbca9e72a
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-10 16:12:02 +0200

    Provisionally disabled thumbnail creation

--- src/main/java/at/yeoman/photobackup/server/imageMagick/ImageMagick.java
+++ src/main/java/at/yeoman/photobackup/server/imageMagick/ImageMagick.java
@@ -5,26 +5,26 @@ import static at.yeoman.photobackup.server.configuration.OperatingSystem.Windows
 
 public class ImageMagick {
     static {
-        if (Windows) {
-            System.loadLibrary("CORE_RL_zlib_");
-            System.loadLibrary("CORE_RL_glib_");
-            System.loadLibrary("CORE_RL_bzlib_");
-            System.loadLibrary("CORE_RL_lcms_");
-            System.loadLibrary("CORE_RL_libxml_");
-            System.loadLibrary("CORE_RL_lqr_");
-            System.loadLibrary("CORE_RL_ttf_");
-            System.loadLibrary("CORE_RL_zlib_");
-            System.loadLibrary("CORE_RL_MagickCore_");
-            System.loadLibrary("CORE_RL_MagickWand_");
-            System.loadLibrary("CORE_RL_Magick++_");
-        } else if (Linux) {
-            System.loadLibrary("MagickCore-7.Q16HDRI");
-            System.loadLibrary("MagickWand-7.Q16HDRI");
-        } else {
-            System.loadLibrary("MagickCore-7");
-            System.loadLibrary("MagickWand-7");
-        }
-        System.loadLibrary("photobackup_server_native");
+//        if (Windows) {
+//            System.loadLibrary("CORE_RL_zlib_");
+//            System.loadLibrary("CORE_RL_glib_");
+//            System.loadLibrary("CORE_RL_bzlib_");
+//            System.loadLibrary("CORE_RL_lcms_");
+//            System.loadLibrary("CORE_RL_libxml_");
+//            System.loadLibrary("CORE_RL_lqr_");
+//            System.loadLibrary("CORE_RL_ttf_");
+//            System.loadLibrary("CORE_RL_zlib_");
+//            System.loadLibrary("CORE_RL_MagickCore_");
+//            System.loadLibrary("CORE_RL_MagickWand_");
+//            System.loadLibrary("CORE_RL_Magick++_");
+//        } else if (Linux) {
+//            System.loadLibrary("MagickCore-7.Q16HDRI");
+//            System.loadLibrary("MagickWand-7.Q16HDRI");
+//        } else {
+//            System.loadLibrary("MagickCore-7");
+//            System.loadLibrary("MagickWand-7");
+//        }
+//        System.loadLibrary("photobackup_server_native");
         
         initialize();
     }
