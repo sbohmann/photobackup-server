751641e99d8e8205ba1f64260c73ba1cd77f1c56
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-10 13:13:49 +0200

    Added warning to almost finished Dockerfile still using ephemeral storage

---
+++ create_storage_links.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+storage_directory=$1
+
+if [[ ! -d "$storage_directory" ]]
+then
+    echo "not a directory: [$storage_directory]"
+    exit 1
+fi
+
+names = assets upload photos thumbnails videos
+
+for name in names
+do
+    subdirectory="$storage_directory/$name"
+    if [[ ! -d "$subdirectory" ]]
+    then
+        echo "not a directory: [$subdirectory]"
+        exit 2
+    fi
+    ln -s "$subdirectory" "$name"
+done
