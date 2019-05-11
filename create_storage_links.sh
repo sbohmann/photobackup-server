5b764485e6af721f6a3ad351f6948847b642b1a2
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-11 09:40:02 +0200

    Removed warning about ephemeral storage from Dockerfile. It now uses a volume or directory provided by the host system.

--- create_storage_links.sh
+++ create_storage_links.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
 storage_directory=$1
 
@@ -8,11 +8,18 @@ then
     exit 1
 fi
 
+sub_path="$storage_directory/backup/photobackup-server"
+
+if [[ ! -d "$sub_path" ]]
+then
+    mkdir -p "$sub_path"
+fi
+
 names="assets upload photos thumbnails videos"
 
 for name in ${names}
 do
-    subdirectory="$storage_directory/$name"
+    subdirectory="$sub_path/$name"
     if [[ ! -d "$subdirectory" ]]
     then
         echo "not a directory: [$subdirectory]"
