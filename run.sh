5c5500088eef9844307494d5917730711e162461
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-05-11 14:35:45 +0200

    Dockerfile for version 1.0.2

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
-version=$(gradle -no-daemon properties -q | grep "version:" | awk '{print $2}') && java -Djava.library.path="libraries:/usr/lib:/usr/local/lib" -jar "build/libs/photobackup-server-${version}.jar"
+echo "fetching photobackup-server gradle version..."
+version=$(gradle -no-daemon properties -q | grep "version:" | awk '{print $2}') | exit 1
+echo "photobackup-server gradle version: $version"
+java -Djava.library.path="libraries:/usr/lib:/usr/local/lib" -jar "build/libs/photobackup-server-${version}.jar"
