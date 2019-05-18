56c99ba06c538f524ae84918ac629feac026eaed
sbohmann <mail@sebastian-bohmann.com>
2019-05-18 14:57:18 +0200

    Password hashing algorithm changed

--- src/main/java/at/yeoman/photobackup/server/PasswordHash.java
+++ src/main/java/at/yeoman/photobackup/server/PasswordHash.java
@@ -16,7 +16,7 @@ class PasswordHash {
     PasswordHash(byte[] salt, String password) throws Exception {
         this.salt = salt;
         this.password = password;
-        md = MessageDigest.getInstance("SHA512");
+        md = MessageDigest.getInstance("SHA-512");
 
         calculateResult();
     }
