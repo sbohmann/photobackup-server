b2a0b83854a2aedfe364bc21110acc5535f1ae2d
sbohmann <mail@sebastian-bohmann.com>
2019-05-18 14:34:41 +0200

    Password cration on startup

---
+++ src/main/java/at/yeoman/photobackup/server/PasswordCreation.java
@@ -0,0 +1,64 @@
+package at.yeoman.photobackup.server;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.io.*;
+import java.security.SecureRandom;
+
+class PasswordCreation {
+    private static final File passwordFile  = new File(Directories.Config, "password.txt");
+
+    private String newPassword;
+    private byte[] salt;
+    private byte[] hash;
+
+    static void createNewPasswordIfMissing() throws Exception {
+        if (!passwordFile.isFile()) {
+            new PasswordCreation().createNewPassword();
+        }
+    }
+
+    private void createNewPassword() throws Exception {
+        readNewPasswordFromPrompt();
+        createSaltAndHash();
+        writePasswordFile();
+    }
+
+    private void createSaltAndHash() throws Exception {
+        createSalt();
+        createHash();
+    }
+
+    private void readNewPasswordFromPrompt() throws IOException {
+        BufferedReader in = createStdinReader();
+        System.out.println("Enter new password:");
+        String password = in.readLine();
+        System.out.println("Repeat new password:");
+        String repeatedPassword = in.readLine();
+
+        if (password.equals(repeatedPassword)) {
+            newPassword = password;
+        }
+    }
+
+    private BufferedReader createStdinReader() {
+        return new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    private void createSalt() {
+        SecureRandom random = new SecureRandom();
+        salt = new byte[256];
+        random.nextBytes(salt);
+    }
+
+    private void createHash() throws Exception {
+        hash = new PasswordHash(salt, newPassword).result;
+    }
+
+    private void writePasswordFile() throws IOException {
+        try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(passwordFile)))) {
+            out.println(Hex.encodeHex(salt));
+            out.println(Hex.encodeHex(hash));
+        }
+    }
+}
---
+++ src/main/java/at/yeoman/photobackup/server/PasswordHash.java
@@ -0,0 +1,34 @@
+package at.yeoman.photobackup.server;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+class PasswordHash {
+    private final byte[] salt;
+    private final String password;
+    private MessageDigest md;
+
+    byte[] result;
+
+    PasswordHash(byte[] salt, String password) throws Exception {
+        this.salt = salt;
+        this.password = password;
+        md = MessageDigest.getInstance("PBKDF2WithHmacSHA1");
+
+        calculateResult();
+    }
+
+    private void calculateResult() throws IOException {
+        md.digest(salt);
+        digestPassword();
+    }
+
+    private void digestPassword() throws IOException {
+        ByteArrayOutputStream utf8EncodedPassword = new ByteArrayOutputStream();
+        new OutputStreamWriter(utf8EncodedPassword, StandardCharsets.UTF_8).write(password);
+        result = md.digest(utf8EncodedPassword.toByteArray());
+    }
+}
