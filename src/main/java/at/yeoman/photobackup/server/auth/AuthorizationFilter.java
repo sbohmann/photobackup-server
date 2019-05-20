bb219d91686adf384445bef3233ff7acc9ecdd95
sbohmann <mail@sebastian-bohmann.com>
2019-05-20 22:47:10 +0200

    Removed insecure logging

--- src/main/java/at/yeoman/photobackup/server/auth/AuthorizationFilter.java
+++ src/main/java/at/yeoman/photobackup/server/auth/AuthorizationFilter.java
@@ -117,12 +117,10 @@ public class AuthorizationFilter implements Filter {
     }
 
     private boolean isValidToken(String key) {
-        log.info("Token: [" + key + "]");
         if (key == null) {
             return false;
         }
         if (!ByteBlock.HexStringPattern.matcher(key).matches()) {
-            log.info("Invalid token format");
             return false;
         }
         return validTokenStoredForId(parse(key));
