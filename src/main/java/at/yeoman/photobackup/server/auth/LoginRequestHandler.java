9a78e33ec0f57d8e7fb66116a338322af3e87442
sbohmann <mail@sebastian-bohmann.com>
2019-05-20 18:37:33 +0200

    Accessing login form

--- src/main/java/at/yeoman/photobackup/server/auth/LoginRequestHandler.java
+++ src/main/java/at/yeoman/photobackup/server/auth/LoginRequestHandler.java
@@ -4,6 +4,7 @@ import at.yeoman.photobackup.server.configuration.SecurityConfiguration;
 import at.yeoman.photobackup.server.primtive.ByteBlock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
